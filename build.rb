bd12b3e12cc7b39cc4e0de8201298fa09e306d00
geemus <geemus@gmail.com>
2011-04-12 13:23:12 -0700

    better progress feedback from build

--- build.rb
+++ build.rb
@@ -1,7 +1,13 @@
+require 'rubygems'
+require 'formatador'
+
 files = Dir.glob('*.markdown')
 
+Formatador.display_line
+
 files.each do |file|
-  filename, _ = file.split('.')
+  filename, _ = file.split('.', 2)
+  Formatador.redisplay("Building: #{filename}")
   source_path = "#{filename}.markdown"
   output_path = "_site/#{filename}.html"
   if File.exists?(output_path)
@@ -9,3 +15,6 @@ files.each do |file|
   end
   `kramdown #{source_path} >> #{output_path}`
 end
+
+Formatador.display_line
+Formatador.display_line
