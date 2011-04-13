d335c200d4e6aca6fcfafec53c5cc2b6061abe62
geemus <geemus@gmail.com>
2011-04-13 16:05:43 -0700

    setup stuff for building .tar.gz files for distribution

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "http://rubygems.org"
+
+gem 'bundler'
+gem 'formatador'
+gem 'kramdown'
--- build.rb
+++ build.rb
@@ -1,24 +1,48 @@
 require 'rubygems'
+require 'bundler'
+require 'bundler/cli'
+require 'fileutils'
 require 'formatador'
-
-files = Dir.glob('*.markdown')
-
 Formatador.display_line
 
-unless File.exists?('_site')
-  Dir.mkdir('_site')
+# nuke old pkg directory if it exists and recreate
+if File.exists?('pkg')
+  FileUtils.rm_rf('pkg')
 end
+Dir.mkdir('pkg')
+Dir.mkdir('pkg/learn_fog')
 
-files.each do |file|
+# compile the markdown files
+Dir.glob('*.markdown').each do |file|
+  Formatador.redisplay("Compiling: #{file}")
   filename, _ = file.split('.', 2)
-  Formatador.redisplay("Building: #{filename}")
   source_path = "#{filename}.markdown"
-  output_path = "_site/#{filename}.html"
+  output_path = "pkg/learn_fog/#{filename}.html"
   if File.exists?(output_path)
     File.delete(output_path)
   end
   `kramdown #{source_path} >> #{output_path}`
 end
 
-Formatador.display_line
-Formatador.display_line
+# copy files from resources
+Dir.glob('resources/*').each do |file|
+  Formatador.redisplay("Copying: #{file}")
+  FileUtils.cp_r(Dir.glob('resources/*'), 'pkg/learn_fog')
+end
+
+# package gems
+Formatador.redisplay("Packaging Gems")
+`BUNDLE_GEMFILE='#{File.expand_path(Dir.pwd)}/pkg/learn_fog/Gemfile' bundle package`
+
+
+# checkout a copy of source
+Formatador.redisplay("Cloning Source")
+Dir.chdir('pkg/learn_fog')
+`git clone -q git://github.com/geemus/fog.git source`
+
+# package learn_fog
+Formatador.redisplay("Packaging")
+Dir.chdir('..')
+`tar -zcvf learn_fog.tar.gz learn_fog`
+
+Formatador.display_lines(['',''])
